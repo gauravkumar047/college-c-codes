300ecc8a26f6a20d19cfde951f774ee5360e20d2
gauravkumar047 <gauravkumar20436@gmail.com>
2024-11-08 09:02:47 +0530

    updated program 15

--- 15_prime_number.c
+++ 15_prime_number.c
@@ -1,8 +1,7 @@
-//15.program to check whether the entered number is prime or not
 #include <stdio.h>	
 int main(){
-    int n, i=3;
-    printf("enter the number: ");
+    int n;
+    printf("enter the number : ");
     scanf("%d",&n);
     if(n<2){
         printf("%d is not a prime number",n);
@@ -10,15 +9,22 @@ int main(){
     else if(n==2){
         printf("%d is a prime number",n);
     }
-    else if(n>2){
-    while(i<n){
-        if((n%i)==0 || (n%2)==0){
-            printf("%d is not a prime number",n);
-            goto a;
-        }
-            i=i+2;
+    else if((n%2)==0){
+        printf("%d is not a prime number",n);
     }
-    printf("%d is a prime number",n);
+    else if(n==3){
+        printf("%d is a prime number",n);
+    }
+    else if(n>3){
+        for(int i=3; i<n; i=i+2){
+            if((n%i)==0){
+                printf("%d is not a prime number",n);
+                goto a;
+            }
+            else{printf("%d is a prime",n);
+            goto a;
+            }
+        }
     }
    a: return 0;
 }
